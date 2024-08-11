f63bb4acb052dadfdf177139c3c4677c7b787cca
Mathesh <mathesh1907@gmail.com>
2024-08-11 17:20:05 +0530

    adding selection sort

--- src/com/Sorting/BubbleSort/BubbleSortAlgorithm.java
+++ src/com/Sorting/BubbleSort/BubbleSortAlgorithm.java
@@ -2,14 +2,16 @@ package com.Sorting.BubbleSort;
 import java.util.Arrays;
 public class BubbleSortAlgorithm {
     public static void main(String[] args) {
-        int[] arr = {5, 4, 3, 2, 1};
+        int[] arr = {1, 2, 3, 4, 5};
         bubble(arr);
         System.out.println(Arrays.toString(arr));
 
     }
     static void bubble(int[] arr){
         // run the steps n-1 times
+        boolean swapped; // juz for knowing if its already sorted or not. // we can remove the boolean if we want.-
         for (int i = 0; i < arr.length; i++) {
+            swapped = false;
             // for each step, the max element come at the last respective index
             for (int j = 1; j < arr.length - i; j++) {
                 // swap if the element is smaller than the previous element
@@ -18,8 +20,11 @@ public class BubbleSortAlgorithm {
                     int temp = arr[j];
                     arr[j] = arr[j-1];
                     arr[j-1] = temp;
+                    swapped = true;
                 }
             }
+            // if you didn't swap for a particular value of i, it means the array is sorted hence stop the program.
+            if (!swapped) break; // its basically  if(swapped == false) { break; } // which means swap should be true. if its false we're giving sorted array as input.
         }
     }
 }
--- src/com/Sorting/SelectionSort/SelectionSortAlgorithm.java
+++ src/com/Sorting/SelectionSort/SelectionSortAlgorithm.java
@@ -1,4 +1,39 @@
 package com.Sorting.SelectionSort;
 
+import com.practice.Array;
+
+import java.util.Arrays;
+
 public class SelectionSortAlgorithm {
+    public static void main(String[] args) {
+        int[] arr = {5, 4, 3, 2, 1};
+        selectionSort(arr);
+        System.out.println(Arrays.toString(arr));
+
+    }
+    static void selectionSort(int[] arr){
+        for (int i = 0; i < arr.length; i++) {
+            // find the max item in the remaining array and swap with correct index
+            int last = arr.length -i -1; // we're juz ignoring the right end sorted elements with -i
+            int MaxIndex = getMatIndex(arr, 0, last);
+            swap(arr, MaxIndex, last);
+        }
+    }
+
+    static void swap(int[] arr, int first, int second){
+        int temp = arr[first]; // its juz swapping the elements
+        arr[first] = arr[second];
+        arr[second] = temp;
+
+    }
+
+    static int getMatIndex(int[] arr, int start, int end) {
+        int max = start;
+        for (int i = start; i <= end ; i++) { // the end in this program 4, so we're checking till 4. 4 <= 4
+            if (arr[i] > arr[max]){
+                max = i; // its juz update the max elements index.
+            }
+        }
+        return max;
+    }
 }
