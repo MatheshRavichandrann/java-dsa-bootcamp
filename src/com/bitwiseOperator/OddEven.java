a2bafb0cde5bbb4df63ee04e8a36f924305b14ad
Mathesh <mathesh1907@gmail.com>
2024-08-20 23:46:09 +0530

    adding OddEven

--- src/com/bitwiseOperator/OddEven.java
+++ src/com/bitwiseOperator/OddEven.java
@@ -1,2 +1,17 @@
-package com.bitwiseOperator;public class OddEven {
+package com.bitwiseOperator;
+
+public class OddEven {
+    public static void main(String[] args) {
+        int n = 22;
+        boolean ans = isOdd(n);
+        if (ans){
+            System.out.println("its odd num");
+        }else {
+            System.out.println("its even num");
+        }
+
+    }
+    static boolean isOdd(int n){
+        return (n & 1) == 1; // if you don't know this have look at notes.
+    }
 }
