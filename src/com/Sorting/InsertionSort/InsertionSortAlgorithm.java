eaaa5a99d2ac023e27adf5e22b26d042ac7690e8
Mathesh <mathesh1907@gmail.com>
2024-09-02 13:55:02 +0530

    addingProblems

--- src/com/Sorting/InsertionSort/InsertionSortAlgorithm.java
+++ src/com/Sorting/InsertionSort/InsertionSortAlgorithm.java
@@ -9,7 +9,7 @@ public class InsertionSortAlgorithm {
     // it's sorting from the 0 th element to n th element. like index verse, 0-1, 0-2, 0-3, 0-4, 0-5.
     static void insertionSort(int[] arr){
         for (int i = 0; i < arr.length -1; i++) { // we're looping through n-2 . we can give i <= arr.length -2 also
-            for (int j = i+1; j > 0 ; j--) { // j should be i+1. we're traveling i+1 to o here which means we're checking left to right. in every iteration j goes and check the left side so we're giving j--
+            for (int j = i+1; j > 0 ; j--) { // j should be i+1. we're traveling i+1 to 0 here which means we're checking left to right. in every iteration j goes and check the left side so we're giving j--
                 if (arr[j] < arr[j-1]){ // checking j is less that j-1(the previous element). if its true then we've to swap the two elements.
                     swap(arr, j, j-1 );
                 }else {
