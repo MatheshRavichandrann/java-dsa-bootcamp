6acfcdc42544ce56077ff0ca8e497658b7df57ad
Mathesh <mathesh1907@gmail.com>
2024-09-13 22:58:34 +0530

    addingProblems

--- src/com/backtracking/AllPaths.java
+++ src/com/backtracking/AllPaths.java
@@ -1,4 +1,7 @@
 package com.backtracking;
+
+import java.util.Arrays;
+
 public class AllPaths {
     public static void main(String[] args) {
         boolean[][] maze = {
@@ -6,7 +9,11 @@ public class AllPaths {
                 {true, true, true},
                 {true, true, true}
         };
-        allPaths("", maze, 0, 0);
+//        allPaths("", maze, 0, 0);
+
+
+        int[][] path = new int[maze.length][maze[0].length];
+        allPathPrint("", maze, 0, 0, path, 1);
     }
 
     static void allPaths(String p, boolean[][] maze, int r, int c){
@@ -45,4 +52,44 @@ public class AllPaths {
     }
 
 
+    static void allPathPrint(String p, boolean[][] maze, int r, int c, int[][] path, int step){
+        if (r == maze.length - 1 && c == maze[0].length - 1){
+            path[r][c] = step;
+            for (int[] arr : path){
+                System.out.println(Arrays.toString(arr));
+            }
+            System.out.println(p);
+            System.out.println();
+            return;
+        }
+
+        if (!maze[r][c]){
+            return;
+        }
+
+        maze[r][c] = false;
+        path[r][c] = step;
+
+
+        if (r < maze.length - 1){
+            allPathPrint(p + 'D', maze, r+1, c, path, step+1);
+        }
+
+        if (c < maze[0].length - 1){
+            allPathPrint(p + 'R', maze, r, c+1, path, step+1);
+        }
+
+        if (r > 0){
+            allPathPrint(p + 'U', maze, r-1, c, path, step+1);
+        }
+
+        if (c > 0){
+            allPathPrint(p + 'L', maze, r, c-1, path, step+1);
+        }
+
+
+        maze[r][c] = true;
+        path[r][c] = 0;
+
+    }
 }
