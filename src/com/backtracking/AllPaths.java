9e78a66f756b5ca60e9cdb92809d517068305635
Mathesh <mathesh1907@gmail.com>
2024-09-15 14:05:53 +0530

    adding Problems

--- src/com/backtracking/AllPaths.java
+++ src/com/backtracking/AllPaths.java
@@ -16,41 +16,6 @@ public class AllPaths {
         allPathPrint("", maze, 0, 0, path, 1);
     }
 
-    static void allPaths(String p, boolean[][] maze, int r, int c){
-        if (r == maze.length - 1 && c == maze[0].length - 1){
-            System.out.println(p);
-            return;
-        }
-
-        if (!maze[r][c]){
-            return;
-        }
-
-        // here the backtracking
-        maze[r][c] = false;
-
-        if (r < maze.length - 1){
-            allPaths(p + 'D', maze, r+1, c);
-        }
-
-        if (c < maze[0].length - 1){
-            allPaths(p + 'R', maze, r, c+1);
-        }
-
-        if (r > 0){
-            allPaths(p + 'U', maze, r-1, c);
-        }
-
-        if (c > 0){
-            allPaths(p + 'L', maze, r, c-1);
-        }
-
-        // this will where the function will be over
-        // so, before the function get removed, also remove the changes that were made by the function
-        maze[r][c] = true;
-
-    }
-
 
     static void allPathPrint(String p, boolean[][] maze, int r, int c, int[][] path, int step){
         if (r == maze.length - 1 && c == maze[0].length - 1){
