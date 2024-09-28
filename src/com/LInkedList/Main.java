ebc9a1747d697b0a826491c0949c55c54f537aa0
Mathesh <mathesh1907@gmail.com>
2024-09-29 00:48:52 +0530

    adding Custom LinkedList

---
+++ src/com/LInkedList/Main.java
@@ -0,0 +1,11 @@
+package com.LInkedList;
+public class Main {
+    public static void main(String[] args) {
+        LL list = new LL();
+        list.insertFirst(3);
+        list.insertFirst(6);
+        list.insertFirst(36);
+        list.insertFirst(69);
+        list.display();
+    }
+}
