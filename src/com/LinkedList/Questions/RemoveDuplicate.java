9777ede281d5ed85806f14a276c6800a2530710b
Mathesh <mathesh1907@gmail.com>
2024-10-13 19:06:45 +0530

    adding new Problems

--- src/com/LinkedList/Questions/RemoveDuplicate.java
+++ src/com/LinkedList/Questions/RemoveDuplicate.java
@@ -14,7 +14,7 @@ public class RemoveDuplicate {
           }
       }
 
-      public ListNode deleteDuplicates(ListNode node){
+      public ListNode deleteDuplicates(ListNode node)   {
           if (node == null){
               return node;
           }
@@ -28,5 +28,38 @@ public class RemoveDuplicate {
           }
           return head;
       }
+
+
+      public ListNode deleteDup(ListNode node){
+          if (node == null){
+              return node;
+          }
+          ListNode head = node;
+          while (node.next != null){
+              if (node.val == node.next.val){
+                  node = node.next.next;
+              }else {
+                  node = node.next;
+              }
+          }
+          return head;
+      }
+
+      public ListNode dupDelete(ListNode node){
+          if (node == null){  // Handle an empty list
+              return node;
+          }
+          ListNode head = node;
+          while (node.next != null){ // Traverse the list
+              if (node.val == node.next.val){ // Found a duplicate
+                  node.next = node.next.next;  // Skip the duplicate
+              }
+              else {
+                  node = node.next;  // Move to the next node
+              }
+          }
+          return head; // Return the updated list without duplicates
+      }
+
 
 }
